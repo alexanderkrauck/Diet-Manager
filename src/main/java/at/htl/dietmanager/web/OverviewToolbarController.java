09bbe7ff54e8517f3a255fe33e0d5678aa11b14b
alexanderkrauck <alexander.krauck@gmail.com>
2018-03-19 19:35:03 +0100

    Improved Overview Page and added ERD

--- src/main/java/at/htl/dietmanager/web/OverviewToolbarController.java
+++ src/main/java/at/htl/dietmanager/web/OverviewToolbarController.java
@@ -1,8 +1,14 @@
 package at.htl.dietmanager.web;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @ManagedBean
+@SessionScoped
 public class OverviewToolbarController {
     private Possibility possibility = Possibility.CreateNewUnspecifiedEatenFood;
 
@@ -13,14 +19,32 @@ public class OverviewToolbarController {
 
     public void clickCreateSpecifiedEatenFood() {
         possibility = Possibility.CreateNewSpecifiedEatenFood;
+        try {
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+        } catch (IOException ex) {
+
+        }
     }
 
     public void clickCreateUnspecifiedEatenFood() {
         possibility = Possibility.CreateNewUnspecifiedEatenFood;
+        try {
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+        } catch (IOException ex) {
+
+        }
     }
 
     public void clickCreateNewFood() {
         possibility = Possibility.CreateNewFood;
+        try {
+            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+            ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+        } catch (IOException ex) {
+
+        }
     }
 
     public Possibility getPossibility() {
